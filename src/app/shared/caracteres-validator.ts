464d849c2e46e47baf5c6a34373d380abe13e652
StevenDesroches <stevendesroches@hotmail.com>
2018-04-13 09:09:34 -0400

    changement du nom de la classe

--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -1,7 +1,7 @@
 import { ValidatorFn } from "@angular/forms";
 import { AbstractControl } from "@angular/forms/src/model";
 
-export class sansEspaces {
+export class validatorCaracter {
     static sansEspaces(): ValidatorFn{
         return (c: AbstractControl): { [key: string]: boolean } | null => {
             if (c.value.trim() == '') {
