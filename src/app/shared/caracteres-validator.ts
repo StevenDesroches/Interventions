e36934227bfaecb62c6151359102e0bdba8dc5ec
StevenDesroches <stevendesroches@hotmail.com>
2018-04-13 09:06:45 -0400

    tp8 code du validateur ajouté,  les tests fonctionnents, la commande .trim() de javascript est utilisé pour détecter les espaces

--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -1,8 +1,12 @@
 import { ValidatorFn } from "@angular/forms";
+import { AbstractControl } from "@angular/forms/src/model";
 
 export class sansEspaces {
     static sansEspaces(): ValidatorFn{
-        return (): { [key: string]: boolean } | null => {
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            if (c.value.trim() == '') {
+                return { 'sansEspaces': false};
+            }
             return {'sansEspaces': true};
         };
     }
