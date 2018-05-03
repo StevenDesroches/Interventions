9afb3d6d69d4113d670b46064dde5409693ebabc
StevenDesroches <stevendesroches@hotmail.com>
2018-05-03 17:35:06 -0400

    tp18

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,33 @@ describe('interventions App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('doit afficher le titre du formulaire Déclarer un problème', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Inter!');
+    expect(page.getParagraphText()).toEqual('Déclarer un problème');
   });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par courriel', () => {
+    page.setChampsValidesScenarioAlternatifParCourriel();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  }); 
+  
+  it('zone DESCRIPTION DU PROBLÈME a une bordure VERTE si nombre de caractères suffisant', () => {
+    page.setZoneDescriptionProblemeCaracteresSuffisants();  
+    expect(page.obtenirClasseZoneDescriptionProbleme()).toContain('is-valid');
+  });
+
+  it('zone NOM DU PRODUIT a une bordure ROUGE si nombre de caractères insuffisant', () => {
+    page.setZoneDescriptionProblemeCaracteresInsuffisants();  
+    expect(page.obtenirClasseZoneDescriptionProbleme()).toContain('is-invalid');
+  }); 
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,52 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/probleme');
   }
 
   getParagraphText() {
-    return element(by.css('Inter-root h1')).getText();
+    return element(by.css('Inter-root h5')).getText();
+  }
+
+  setChampsValidesScenarioNominal() : void {
+    element(by.id('inputPrenom')).sendKeys('Steven Desroches');
+    element(by.id('inputNom')).sendKeys('Desroches');    
+    // Sélectionner le premier élément dans la zone de liste déroulante
+    element(by.id('noTypeProblemeId')).all(by.tagName('option')).get(2).click();      
+    // Cliquer sur la première option du bouton radio
+    element.all(by.id('notificationId')).get(0).click();
+    
+    element(by.id('noUniteId')).sendKeys('1');
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');  
+  }
+
+  setChampsValidesScenarioAlternatifParMessageTexte() : void {  
+    element.all(by.id('notificationId')).get(2).click();
+    element(by.id('telephoneId')).sendKeys('5141231234');
+  }
+
+  setChampsValidesScenarioAlternatifParCourriel() : void {  
+    element.all(by.id('notificationId')).get(1).click();
+    element(by.id('courrielId')).sendKeys('aa@bbb.com');
+    element(by.id('courrielConfirmationId')).sendKeys('aa@bbb.com');
+  }
+
+  boutonSubmit() : ElementFinder { 
+    return element(by.buttonText('Sauvegarder'));
+  }
+
+  setZoneDescriptionProblemeCaracteresSuffisants() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('Ceci est un message de description valide');
+  }
+
+  setZoneDescriptionProblemeCaracteresInsuffisants() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('none');
+  }
+
+  obtenirClasseZoneDescriptionProbleme()   { 
+    return element(by.id('descriptionProblemeId')).getAttribute("class");
   }
 }
